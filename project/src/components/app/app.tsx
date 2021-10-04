2d91aadd5507791b4379c81738036fa64cf4c339
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-10-04 23:24:06 +0300

    Описал маршруты в приложении

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,27 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainPage from '../main-page/main-page';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import PropertyScreenNotLogged from '../property-screen-not-logged/property-screen-not-logged';
+
 
 type AppProps = {
   placeCardCount: number;
 }
 
 function App({ placeCardCount }: AppProps): JSX.Element {
-  return <MainPage placeCardCount={placeCardCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage placeCardCount={placeCardCount} />
+        </Route>
+        <Route path="/login" exact component={LoginScreen} />
+        <Route path="/favorites" exact component={FavoritesScreen} />
+        <Route path="/offer/:id" exact component={PropertyScreenNotLogged} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
