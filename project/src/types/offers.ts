cfc599dab0b2c99828b14c9a07ef365341de376f
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-10-08 22:55:09 +0300

    Создал моки карточек предложений и отзывов

---
+++ project/src/types/offers.ts
@@ -0,0 +1,21 @@
+type Offers = {
+  bedrooms: number,
+  description: string,
+  goods: string[],
+  hostAvatarUrl: string,
+  hostId: number,
+  hostIsPro: boolean,
+  hostName: string,
+  id: number,
+  images: string[],
+  isFavorite: boolean,
+  isPremium: boolean,
+  maxAdults: number,
+  previewImage: string,
+  price: number,
+  rating: number,
+  title: string,
+  type: string,
+};
+
+export type { Offers };
---
+++ project/src/types/reviews.ts
@@ -0,0 +1,14 @@
+type Reviews = {
+  offersID: number,
+  comment: string,
+  date: string,
+  id: number,
+  rating: number,
+  userAvatarUrl: string,
+  userId: number,
+  userIsPro: boolean,
+  userName: string
+};
+
+export type { Reviews };
+
