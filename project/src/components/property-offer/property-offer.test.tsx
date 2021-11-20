3d19c1476c8de9893c47c482c81beed63b55d062
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-11-20 15:14:26 +0300

    Написал тест для PropertyOffer

---
+++ project/src/components/property-offer/property-offer.test.tsx
@@ -0,0 +1,45 @@
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { makeFakeOffers, makeFakeReviews } from '../../utils/moks';
+import { render, screen } from '@testing-library/react';
+import { createAPI } from '../../services/api';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { State } from '../../types/state';
+import { Action } from 'redux';
+import { Provider } from 'react-redux';
+import { AuthorizationStatus } from '../../const';
+import PropertyOffer from './property-offer';
+
+const history = createMemoryHistory();
+const mockOffers = makeFakeOffers().slice(0, 1);
+const mockComments = makeFakeReviews().slice(0, 1);
+const onFakeUnauthorized = jest.fn();
+const api = createAPI(onFakeUnauthorized());
+const middlewares = [thunk.withExtraArgument(api)];
+const mockStore = configureMockStore<
+  State,
+  Action,
+  ThunkDispatch<State, typeof api, Action>
+>(middlewares);
+
+const store = mockStore({
+  USER: { authorizationStatus: AuthorizationStatus.Auth },
+  DATA: { offer: mockOffers[0], comments: mockComments, nearbyOffers: mockOffers },
+});
+
+describe('Component: PropertyOffer', () => {
+
+  it('should render correctly', () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <PropertyOffer />
+        </Router>
+      </Provider>);
+
+    expect(screen.getByText(mockOffers[0].title)).toBeInTheDocument();
+    expect(screen.getByText(/Meet the host/i)).toBeInTheDocument();
+    expect(screen.getByRole('dialog')).toBeInTheDocument();
+  });
+});
